70b9ead4ce99f5e9856912d12524001c4ab6c83b
kristianklimek <klimekkristian@gmail.com>
2018-11-01 11:27:32 +0100

    hello-for cyklus

---
+++ index.php
@@ -0,0 +1,32 @@
+
+<!DOCTYPE html>
+<html lang="cs" dir="ltr">
+  <head>
+    <link rel="stylesheet" href="styly/styly.css">
+    <meta charset="utf-8">
+    <title>kok</title>
+  </head>
+  <body>
+    <form action="submit.php" method="post">
+<?php
+//for ($index=1; $index <= 100; $index ++) {
+  //echo $index . "<br>";
+  // code...
+//}
+//for ($i=0; $i<100; $i=$i+5) {
+  // code...
+  //echo $i . "<br>";
+//}
+for ($i=100; $i > 0 ; $i=$i-5) {
+  // code...
+  echo $i . "<br>";
+}
+
+?>
+
+
+ <br >Copyright (c) 2018 Copyright Holder All Rights Reserved.
+
+      </form>
+    </body>
+  </html>
